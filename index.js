6a7f0ee4fdc12fa113f1eab4e922889efd4f0ba8
Enej Licina <elicina@nbeyond.nl>
2022-11-04 16:14:27 +0100

    trying updating, failed

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ const sloTech = ""
 const zrsz = "https://www.iskanjedela.si/search-jobs?jobId=2906410"
 const tujina = ""
 
-// creating the array with the job listings
-const jobListings = []
+// defining the base URL's
+const mojeDeloBaseUrl = 'https://www.mojedelo.com/'
+
+const jobListings = [] // <- creating the array with the job listings
+let indexPage = 1
+let totalPages = 1
+let maxPages = 1
 
 const app = express()
-
-// making sure our app uses cors
-app.use(cors())
-
-// app.get('/', function(req, res) {
-//     res.json("This is my webscrapper")
-// })
+app.use(cors()) // <- making sure our app uses cors
 
 app.get('/', function(req, res) {
     axios(mojeDelo)
@@ -30,21 +29,10 @@ app.get('/', function(req, res) {
             const html = response.data
             const $ = cheerio.load(html);
 
-            // $(".w-inline-block.job-ad.deluxe.w-clearfix .details .title", html).each(function(){
-            //     const jobTitle = $(this).text()
-            //     jobListings.push(jobTitle)
-            // })
-            //
-            // $(".w-inline-block.job-ad.deluxe.w-clearfix .details p", html).each(function(){
-            //     const jobDescription = $(this).text()
-            //     jobListings.push(jobDescription)
-            // })
-
             $(".w-inline-block.job-ad.deluxe.w-clearfix", html).each(function(){
                 const jobTitle = $(this).find('h2.title').text()
-                const baseUrl = 'https://www.mojedelo.com/'
                 const url = $(this).attr('href')
-                const jobLink = baseUrl + url
+                const jobLink = mojeDeloBaseUrl + url
                 const companyName = $(this).find('.boxName .detail').text()
                 const jobLocation = $(this).find('.box-details-icon.icon-map-marker + .detail').text()
                 const datePosted = $(this).find('.box-details-icon.icon-calendar + .detail').text()
